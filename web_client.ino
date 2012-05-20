f1e6ede9b59e6eb7b9ef93a6a7251a5c386e260a
Matt <matt@primestudiosllc.com>
2012-05-19 19:19:38 -0700

    changed the ethernet connection methods

--- web_client.ino
+++ web_client.ino
@@ -1,15 +1,14 @@
-/*
-  Web client
- 
- based off code by David A. Mellis
- 
- */
-
-#include <SPI.h>
 #include <Ethernet.h>
+#include <SPI.h>
 #include "init.h"
 
-//mac and server built from init file
+//init file helps build some of these variables
+IPAddress server(  SERVER_BYTE_1,
+                   SERVER_BYTE_2,
+                   SERVER_BYTE_3,
+                   SERVER_BYTE_4
+);
+
 byte mac[] = {  MAC_BYTE_1, 
                 MAC_BYTE_2, 
                 MAC_BYTE_3, 
@@ -18,62 +17,74 @@ byte mac[] = {  MAC_BYTE_1,
                 MAC_BYTE_6
 };
 
-IPAddress server(  SERVER_BYTE_1,
-                   SERVER_BYTE_2,
-                   SERVER_BYTE_3,
-                   SERVER_BYTE_4
-);
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
-void setup() {
-  // start the serial library:
+boolean connectFail = false;  //flag for a connection failure
+boolean startRead = false;    // if the program is reading data
+char inString[32];            // string for incoming serial data
+int stringPos = 0;            // string index counter
+
+void setup(){
+  Ethernet.begin(mac);
   Serial.begin(9600);
-  // start the Ethernet connection:
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // no point in carrying on, so do nothing forevermore:
-    for(;;)
-      ;
+}
+
+void loop(){
+  String pageValue = connectAndRead(); //connect to the server and read the output
+  if(connectFail) {
+    //handle error
+  } else {
+
   }
-  // give the Ethernet shield a second to initialize:
-  delay(1000);
+  Serial.println(pageValue); //print out the findings.
+  delay(15000); //wait 5 seconds before connecting again
+  connectFail = false;
+}
+
+String connectAndRead(){
+  //connect to the server
   Serial.println("connecting...");
 
-  // if you get a connection, report back via serial:
   if (client.connect(server, 80)) {
     Serial.println("connected");
-    // Make a HTTP request:
     client.println(REQUEST_STRING);
     client.println(REQUEST_HOST);
     client.println();
-  } 
-  else {
-    // kf you didn't get a connection to the server:
-    Serial.println("connection failed");
+
+    //Connected - Read the page
+    return readPage();
+  }else{
+    connectFail = 1;
+    return "connection failed";
   }
+
 }
 
-void loop()
-{
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
+//capture & return integer between brackets
+String readPage(){
+  stringPos = 0;
+  memset( &inString, 0, 32 ); //clear inString memory
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
+  while(true){
 
-    // do nothing forevermore:
-    for(;;)
-      ;
+    if (client.available()) {
+      char c = client.read();
+
+      if (c == '{' ) {
+        startRead = true;
+      } else if(startRead) {
+        if(c != '}') {
+          inString[stringPos] = c;
+          stringPos ++;
+        } else {
+          //got everything
+          startRead = false;
+          client.stop();
+          client.flush();
+          Serial.println("disconnecting.");
+          return int(inString);
+        }
+      }
+    }
   }
 }
